a7213fa27362bbb339d80cc949512d4240683e83
MatanWaisman <matan.waisman@gmail.com>
2020-03-30 15:15:39 +0300

    Updates

---
+++ precache-manifest.ca557bb949b262c0c1c1d3bc3497b2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d8bec1984732640a6fd35415532129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "974626630ca0e5c4ceb9",
+    "url": "/static/css/main.2f341555.chunk.css"
+  },
+  {
+    "revision": "5bd66d21a9312a9ce521",
+    "url": "/static/js/2.580a469d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.580a469d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974626630ca0e5c4ceb9",
+    "url": "/static/js/main.670bb26e.chunk.js"
+  },
+  {
+    "revision": "20c097d77ab757c2e63e",
+    "url": "/static/js/runtime-main.bda5f2b6.js"
+  }
+]);
